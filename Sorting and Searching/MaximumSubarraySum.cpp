1bf0234a80e5d1044fb95d1649af84d109d5689b
KshitijVaidya <kshitijvaidya16@gmail.com>
2024-06-03 12:39:15 +0530

    Editted MaximumSubarraySum.cpp

--- Sorting and Searching/MaximumSubarraySum.cpp
+++ Sorting and Searching/MaximumSubarraySum.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<climits>
 
 using namespace std;
 typedef long long ll;
@@ -7,12 +8,13 @@ typedef long long ll;
 int main(){
     ll n; cin >> n;
 
-    ll max_sum = 0, current_sum = 0;
+    ll max_sum = LLONG_MIN, current_sum = 0;
     while(n--){
         ll i; cin >> i;
-        current_sum = max(i, current_sum + i);
+        current_sum += i;
         max_sum = max(max_sum, current_sum);
+        if(current_sum < 0) current_sum = 0;
     }
-    cout << max_sum << '\n';
+    cout <<  max_sum << endl;
     return 0;
 }
